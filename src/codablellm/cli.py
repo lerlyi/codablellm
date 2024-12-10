dec119fefb77987e718b0c98eef01391e6eb5f9d
Dylan Manuel <dylan.manuel@att.net>
2024-12-09 22:06:24 -0600

    download remote repositories

--- src/codablellm/cli.py
+++ src/codablellm/cli.py
@@ -1,8 +1,23 @@
-from typer import Typer
+from codablellm.core import downloader
+from pathlib import Path
+from typer import Argument, Option, Typer
+from typing import Final
 
 
 app = Typer()
 
+REPO: Final[Path] = Argument(help='Path to the local repository.', file_okay=False, exists=True,
+                             show_default=False)
+GIT: Final[bool] = Option(False, '--git / --archive', help='Determines whether --url is a Git '
+                          'download URL or a tarball/zipfile download URL.')
+URL: Final[str] = Option('', help='Download a remote repository and save at the local path '
+                         'specified by the REPO argument.')
+
+
 @app.command()
-def command():
-    pass
+def command(repo: Path = REPO, git: bool = GIT, url: str = URL):
+    if url:
+        if git:
+            downloader.clone(url, repo)
+        else:
+            downloader.decompress(url, repo)
---
+++ src/codablellm/core/utils.py
@@ -0,0 +1,19 @@
+def get_readable_file_size(size: int) -> str:
+    '''
+    Converts number of bytes to a human readable output (i.e. bytes, KB, MB, GB, TB.)
+
+    Parameters:
+        size: The number of bytes.
+
+    Returns:
+        A human readable output of the number of bytes.
+    '''
+    kb = round(size / 2 ** 10, 3)
+    mb = round(size / 2 ** 20, 3)
+    gb = round(size / 2 ** 30, 3)
+    tb = round(size / 2 ** 40, 3)
+
+    for measurement, suffix in [(tb, 'TB'), (gb, 'GB'), (mb, 'MB'), (kb, 'KB')]:
+        if measurement >= 1:
+            return f'{measurement} {suffix}'
+    return f'{size} bytes'
